79fd70934f8f4c4aaa7bff74e6a0fe203a779b55
Sena <senodetech@gmail.com>
2026-07-04 18:20:22 +0530

    feat: attach websocket server to http server

--- projects/Day-01-WebSocket-Broadcast/backend/server.js
+++ projects/Day-01-WebSocket-Broadcast/backend/server.js
@@ -1,1 +1,21 @@
-console.log("Day 1 - WebSocket Broadcast Server");
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ 
+    server
+});
+
+app.get("/", (req, res) => {
+    res.send("Day 1 - Architect Journey");
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`Listening started on port ${PORT}`)
+});
